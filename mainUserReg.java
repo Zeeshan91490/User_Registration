ba7386f39746026b5c464d5ac04c4c46f37dfaed
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-05 01:09:49 +0530

    mainUserReg.java

--- mainUserReg.java
+++ mainUserReg.java
@@ -1,16 +1,65 @@
 package UserReg;
+import java.util.*;
+import java.util.regex.Pattern;
 
+public class uc3UserReg {
+	
+        private String firstName;
+		private String lastName;
+		private String emailId;
 
-public class mainUserReg {
-	public static void main(String[] args){
-	      System.out.println("Welcome to User Registration System which ensures all validations are in place during entry.");
-	
-	}
-	
-	
-	
-	
-	
-	
+		private void addFirstName() {
+			boolean validCounter = false;
+			Scanner sc = new Scanner(System.in);
+			do {
+				System.out.println("First Name: ");
+				firstName = sc.nextLine();
+				if(Pattern.matches("^[A-Z]{1}[a-z]{2,}$", firstName)) {
+					validCounter = true;
+					System.out.println("Valid!");
+				}
+				else
+					System.out.println("Invalid. Please enter again!");
+			} while(!validCounter);
+		}
+		
+		private void addLastName() {
+			boolean validCounter = false;
+			Scanner sc = new Scanner(System.in);
+			do {
+				System.out.println("Last Name: ");
+				lastName = sc.nextLine();
+				if(Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lastName)) {
+					validCounter = true;
+					System.out.println("Valid!");
+				}
+				else
+					System.out.println("Invalid. Please enter again!");
+			} while(!validCounter);
+		}
+		
+		private void addEmail() {
+			boolean validCounter = false;
+			Scanner sc = new Scanner(System.in);
+			do {
+				System.out.println("Email ID: ");
+				emailId = sc.nextLine();
+				if(Pattern.matches("^[a-zA-Z0-9_]+([.+-]{1}[a-zA-Z0-9_]+)*[@]{1}[a-zA-Z0-9]+[.]{1}[a-zA-Z0-9]{2,}([.]{1}[a-zA-Z]{2,})?$", emailId)) {
+					validCounter = true;
+					System.out.println("Valid!");
+				}
+				else
+					System.out.println("Invalid. Please enter again!");
+			} while(!validCounter);
+			sc.close();
+		}
+		
+		public static void main(String[] args) {
+			uc3UserReg userOne = new uc3UserReg();
+			userOne.addFirstName();
+			userOne.addLastName();
+			userOne.addEmail();
+		}
+
 }
-	
+
