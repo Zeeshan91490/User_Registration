dc4aa6cfdeccbc23b65045132b153ee9bf290aab
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-05 01:13:12 +0530

    mainUserReg.java

--- mainUserReg.java
+++ mainUserReg.java
@@ -1,16 +1,100 @@
 package UserReg;
+import java.util.*;
+import java.util.regex.Pattern;
 
 
-public class mainUserReg {
-	public static void main(String[] args){
-	      System.out.println("Welcome to User Registration System which ensures all validations are in place during entry.");
-	
+public class uc5UserReg {
+	private String firstName;
+	private String lastName;
+	private String emailId;
+	private String mobileNo;
+	private String password;
+
+	private void addFirstName() {
+		boolean validCounter = false;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("First Name: ");
+			firstName = sc.nextLine();
+			if(Pattern.matches("^[A-Z]{1}[a-z]{2,}$", firstName)) {
+				validCounter = true;
+				System.out.println("Valid!");
+			}
+			else
+				System.out.println("Invalid. Please enter again!");
+		} while(!validCounter);
 	}
-	
-	
-	
-	
-	
-	
+
+	private void addLastName() {
+		boolean validCounter = false;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("Last Name: ");
+			lastName = sc.nextLine();
+			if(Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lastName)) {
+				validCounter = true;
+				System.out.println("Valid!");
+			}
+			else
+				System.out.println("Invalid. Please enter again!");
+		} while(!validCounter);
+	}
+
+	private void addEmail() {
+		boolean validCounter = false;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("Email ID: ");
+			emailId = sc.nextLine();
+			if(Pattern.matches("^[a-zA-Z0-9_]+([.+-]{1}[a-zA-Z0-9_]+)*[@]{1}[a-zA-Z0-9]+[.]{1}[a-zA-Z0-9]{2,}([.]{1}[a-zA-Z]{2,})?$", emailId)) {
+				validCounter = true;
+				System.out.println("Valid!");
+			}
+			else
+				System.out.println("Invalid. Please enter again!");
+		} while(!validCounter);
+	}
+
+	private void addMobile() {
+		boolean validCounter = false;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("Mobile No: ");
+			mobileNo = sc.nextLine();
+			if(Pattern.matches("^[0-9]{2}[ ][0-9]{10}$", mobileNo)) {
+				validCounter = true;
+				System.out.println("Valid!");
+			}
+			else
+				System.out.println("Invalid. Please enter again!");
+		} while(!validCounter);
+	}
+
+	private void addPassword() {
+		boolean validCounter = false;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("Password: ");
+			password = sc.nextLine();
+			if(Pattern.matches("^.{8,}$", password)) {
+				validCounter = true;
+				System.out.println("Valid!");
+			}
+			else
+				System.out.println("Invalid. Please enter again!");
+		} while(!validCounter);
+	}
+
+	public static void main(String[] args) {
+		uc5UserReg  userOne = new uc5UserReg  ();
+		userOne.addFirstName();
+		userOne.addLastName();
+		userOne.addEmail();
+		userOne.addMobile();
+		userOne.addPassword();
+	}
+
 }
-	
+
+
+
