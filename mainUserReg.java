4bbe5fc296d7f9a3515cac68c5423a1a143ef603
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-05 01:08:01 +0530

    mainUserReg.java

--- mainUserReg.java
+++ mainUserReg.java
@@ -1,16 +1,50 @@
 package UserReg;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class uc2UserReg {
 
 
-public class mainUserReg {
-	public static void main(String[] args){
-	      System.out.println("Welcome to User Registration System which ensures all validations are in place during entry.");
 	
-	}
-	
-	
-	
-	
-	
-	
+		private String firstName;
+		private String lastName;
+
+		private void addFirstName() {
+			boolean validCounter = false;
+			Scanner sc = new Scanner(System.in);
+			do {
+				System.out.println("First Name: ");
+				firstName = sc.nextLine();
+				if(Pattern.matches("^[A-Z]{1}[a-z]{2,}$", firstName)) {
+					validCounter = true;
+					System.out.println("Valid!");
+				}
+				else
+					System.out.println("Invalid. Please enter again!");
+			} while(!validCounter);
+		}
+		
+		private void addLastName() {
+			boolean validCounter = false;
+			Scanner sc = new Scanner(System.in);
+			do {
+				System.out.println("Last Name: ");
+				lastName = sc.nextLine();
+				if(Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lastName)) {
+					validCounter = true;
+					System.out.println("Valid!");
+				}
+				else
+					System.out.println("Invalid. Please enter again!");
+			} while(!validCounter);
+			sc.close();
+		}
+		public static void main(String[] args) {
+			uc2UserReg userOne = new uc2UserReg();
+			userOne.addFirstName();
+			userOne.addLastName();
+		}
+
 }
+
 	
