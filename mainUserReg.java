b20a56472b5e43a8792e03b4edac6acbc4b2f550
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-05 01:00:52 +0530

    mainUserReg.java

---
+++ mainUserReg.java
@@ -0,0 +1,16 @@
+package UserReg;
+
+
+public class mainUserReg {
+	public static void main(String[] args){
+	      System.out.println("Welcome to User Registration System which ensures all validations are in place during entry.");
+	
+	}
+	
+	
+	
+	
+	
+	
+}
+	
